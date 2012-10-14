ae32325db0f866fc2bfd36ae6a08d679fa1f08e0
hotveryspicy <hotnspicy4@gmail.com>
2012-10-14 12:40:56 +0530

    Files added

---
+++ src/com/hotveryspicy/imageviewgif/ImageViewGIF.java
@@ -0,0 +1,72 @@
+package com.hotveryspicy.imageviewgif;
+
+import java.io.InputStream;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Movie;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class ImageViewGIF extends ImageView{
+	InputStream mInputStream;
+	Movie mMovie;
+	long movieStart;
+	RuntimeException mException = null;
+	int gifSource=0;
+	
+	public ImageViewGIF(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.GIF, 0, 0);
+		gifSource = a.getResourceId(R.styleable.GIF_image, 0);
+		if (gifSource == 0) {
+			mException = new IllegalArgumentException(a.getPositionDescription() + 
+					": The content attribute is required and must refer to a valid image.");
+		}
+		
+		if (mException != null) 
+			throw mException;
+		
+		mInputStream= getContext().getResources().openRawResource(gifSource);
+		mMovie = Movie.decodeStream(mInputStream);
+		a.recycle();
+		
+	}
+	
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+
+	canvas.drawColor(Color.BLACK);
+	super.onDraw(canvas);
+	long now=android.os.SystemClock.uptimeMillis();
+//	System.out.println("now="+now);
+	if (movieStart == 0) { // first time
+		movieStart = now;
+
+	}
+//	System.out.println("\tmoviestart="+movieStart);
+	int relTime = (int)((now - movieStart) % mMovie.duration()) ;
+//	System.out.println("time="+relTime+"\treltime="+mMovie.duration());
+	mMovie.setTime(relTime);
+	mMovie.draw(canvas,0,0);
+	this.invalidate();
+	}
+
+//	@Override
+//	protected void onDraw(Canvas canvas) {
+//		canvas.drawColor(Color.TRANSPARENT);
+//		super.onDraw(canvas);
+//		long now = android.os.SystemClock.uptimeMillis();
+//		if (movieStart == 0) {
+//			movieStart = now;
+//		}
+//		if (mMovie != null) {
+//			int relTime = (int) ((now - movieStart) % mMovie.duration());
+//			mMovie.setTime(relTime);
+//			mMovie.draw(canvas, 0, 0);
+//			this.invalidate();
+//		}
+//	}
+}
---
+++ src/com/hotveryspicy/imageviewgif/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hotveryspicy.imageviewgif;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
